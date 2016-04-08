890f245d7bdbaf23feb6eb0acf18dcc8f2335cf0
tu4 <tu4e4ka096@gmail.com>
2016-04-08 18:17:55 +0300

    change App.java

--- src/main/java/uits/jv1604/algoritms/App.java
+++ src/main/java/uits/jv1604/algoritms/App.java
@@ -1,7 +1,18 @@
 package uits.jv1604.algoritms;
 
+import java.util.Arrays;
+
 /**
  * Created by JV on 4/8/2016
  */
 public class App {
+    public static void main(String[] args) {
+        int[]ms = new int[25];
+        int k = 5;
+        for (int i = 0;i < ms.length;i++){
+            ms[i] = k;
+            k+=10;
+        }
+        System.out.println(Arrays.toString(ms));
+    }
 }
